b8d4f70c3bd10a376d175e594bd2d6c594d9e300
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-08-30 21:48:44 -0300

    Refactor messangi to follow base repository

--- handlers/messangi/messangi.go
+++ handlers/messangi/messangi.go
@@ -8,12 +8,13 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/nyaruka/courier"
-	"github.com/nyaruka/courier/handlers"
 	"encoding/base64"
 	"encoding/hex"
-	"github.com/nyaruka/courier/utils"
 	"encoding/xml"
+
+	"github.com/nyaruka/courier"
+	"github.com/nyaruka/courier/handlers"
+	"github.com/nyaruka/courier/utils"
 )
 
 const (
@@ -54,9 +55,9 @@ func (h *handler) Initialize(s courier.Server) error {
 //	<description>Completed</description>
 //</response>
 type mtResponse struct {
-	Input		string `xml:"input"`
-	Status		string `xml:"status"`
-	Description	string `xml:"description"`
+	Input       string `xml:"input"`
+	Status      string `xml:"status"`
+	Description string `xml:"description"`
 }
 
 // SendMsg sends the passed in message, returning any error
@@ -84,12 +85,12 @@ func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStat
 	status := h.Backend().NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgErrored)
 	parts := handlers.SplitMsg(handlers.GetTextAndAttachments(msg), maxMsgLength)
 	for _, part := range parts {
-		shortcode  := strings.TrimPrefix(msg.Channel().Address(), "+")
-		to         := strings.TrimPrefix(msg.URN().Path(), "+")
+		shortcode := strings.TrimPrefix(msg.Channel().Address(), "+")
+		to := strings.TrimPrefix(msg.URN().Path(), "+")
 		textBase64 := base64.RawURLEncoding.EncodeToString([]byte(part))
-		params     := fmt.Sprintf("%d/%s/%d/%s/%s", instanceId, shortcode, carrierId, to, textBase64)
-		signature  := signHMAC256(privateKey, params)
-		fullURL    := fmt.Sprintf("%s/%s/%s/%s", sendURL, params, publicKey, signature)
+		params := fmt.Sprintf("%d/%s/%d/%s/%s", instanceId, shortcode, carrierId, to, textBase64)
+		signature := signHMAC256(privateKey, params)
+		fullURL := fmt.Sprintf("%s/%s/%s/%s", sendURL, params, publicKey, signature)
 
 		req, _ := http.NewRequest(http.MethodGet, fullURL, nil)
 		rr, err := utils.MakeHTTPRequest(req)

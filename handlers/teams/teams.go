bcb1da33445165bf747a1f45d9b71d24af09dfb0
Robi9 <robertasousa714@gmail.com>
2024-02-28 18:45:44 -0300

    Send image link in message text

--- handlers/teams/teams.go
+++ handlers/teams/teams.go
@@ -397,10 +397,10 @@ func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStat
 		// Process each attachment separately
 		mimeType, attURL := handlers.SplitAttachment(attachment)
 		attType := strings.Split(mimeType, "/")[0]
-		filename, err := utils.BasePathForURL(attURL)
-		if err != nil {
-			logrus.WithField("channel_uuid", msg.Channel().UUID().String()).WithError(err).Error("Error while parsing the media URL")
-		}
+		//filename, err := utils.BasePathForURL(attURL)
+		// if err != nil {
+		// 	logrus.WithField("channel_uuid", msg.Channel().UUID().String()).WithError(err).Error("Error while parsing the media URL")
+		// }
 
 		if attType == "application" {
 			attType = "document"
@@ -408,15 +408,17 @@ func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStat
 
 		// Create a new payload for each attachment
 		attPayload := Activity{Type: "message"}
-		if attType == "video" || attType == "document" || attType == "audio" {
+		if attType == "video" || attType == "document" || attType == "audio" || attType == "image" {
 			attPayload.Text = attURL
-		} else {
+		}
+		// make code snippet unusable while image upload is not working
+		/*else {
 			attPayload.Attachments = append(attPayload.Attachments, Attachment{mimeType, attURL, filename, struct {
 				DownloadUrl string "json:\"downloadUrl,omitempty\""
 				UniqueId    string "json:\"uniqueId,omitempty\""
 				FileType    string "json:\"fileType,omitempty\""
 			}{}})
-		}
+		}*/
 
 		payloadArray = append(payloadArray, attPayload)
 	}

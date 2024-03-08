eee142d2fafdfd7d8c317f29947d998c7fc7abfe
Rafael Soares <rafael.dvlpr@gmail.com>
2024-03-08 16:51:42 -0300

    add billing msg Send implementation for rabbitmq

---
+++ billing/billing.go
@@ -0,0 +1,97 @@
+package billing
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/pkg/errors"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const QUEUE_NAME = "billing_message"
+
+// Message represents a object that is sent to the billing service
+//
+//	{
+//		  "contact_uuid": "69625dca-7922-477c-97c6-9dae8ffff46d",
+//		  "channel_uuid": "9d24bce2-145f-4e65-b9ed-72ef19ee81e0",
+//		  "message_uuid": "dc71bdf1-bb97-4c7d-a88d-b924dc9f414b",
+//		  "message_date": "2024-03-08T16:08:19-03:00"
+//	 }
+type Message struct {
+	ContactUUID string `json:"contact_uuid,omitempty"`
+	ChannelUUID string `json:"channel_uuid,omitempty"`
+	MessageUUID string `json:"message_uuid,omitempty"`
+	MessageDate string `json:"message_date,omitempty"`
+}
+
+// Create a new message
+func NewMessage(contactUUID, channelUUID, messageUUID, messageDate string) *Message {
+	return &Message{
+		ContactUUID: contactUUID,
+		ChannelUUID: channelUUID,
+		MessageDate: messageDate,
+		MessageUUID: messageUUID,
+	}
+}
+
+// Client represents a client interface for billing service
+type Client interface {
+	Send(msg Message) error
+}
+
+// rabbitmqClient represents struct that implements billing service client interface
+type rabbitmqClient struct {
+	channel *amqp.Channel
+	queue   *amqp.Queue
+}
+
+// NewRMQBillingClient creates a new billing service client implementation using RabbitMQ
+func NewRMQBillingClient(conn *amqp.Connection) (Client, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open a channel to rabbitmq")
+	}
+	q, err := ch.QueueDeclare(
+		QUEUE_NAME,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to declare a queue for billing publisher")
+	}
+	return &rabbitmqClient{
+		queue:   &q,
+		channel: ch,
+	}, nil
+}
+
+// Send sends a message to Billing service
+func (c *rabbitmqClient) Send(msg Message) error {
+	msgMarshalled, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	err = c.channel.PublishWithContext(
+		ctx,
+		"",
+		c.queue.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        msgMarshalled,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ billing/billing_test.go
@@ -0,0 +1,69 @@
+package billing
+
+import (
+	"encoding/json"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBillingClient(t *testing.T) {
+
+	conn, err := amqp.Dial("amqp://localhost:5672/")
+	assert.NoError(t, err)
+	billingClient, err := NewRMQBillingClient(conn)
+	assert.NoError(t, err)
+
+	msgUUID, _ := uuid.NewV4()
+
+	msg := NewMessage(
+		"02a6abf4-2145-4a2d-bf71-62d4039a2586",
+		"64a75af3-7e8d-41a5-8ef8-c273056c4fca",
+		msgUUID.String(),
+		time.Now().Format(time.RFC3339),
+	)
+
+	err = billingClient.Send(*msg)
+	assert.NoError(t, err)
+
+	ch, err := conn.Channel()
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "failed to declare a channel for consumer"))
+	}
+	msgs, err := ch.Consume(
+		QUEUE_NAME,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "failed to declare consumer"))
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	var cmsg Message
+
+	go func() {
+		for d := range msgs {
+			err := json.Unmarshal(d.Body, &cmsg)
+			if err != nil {
+				t.Error(errors.Wrap(err, "failed to unmarshal"))
+			}
+
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
+	assert.Equal(t, cmsg.MessageUUID, msg.MessageUUID)
+}
